471efec203a9f16f018c54078459944116b3b7fe
tangct <670992207@qq.com>
2022-03-08 11:33:49 +0800

    first commit

---
+++ precache-manifest.f6a384bfeec60547461482b1c491858b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dc8e4df81d44e19667",
+    "url": "/simulation-platform/css/app.16f6b218.css"
+  },
+  {
+    "revision": "3198f9f80fd0e564c050",
+    "url": "/simulation-platform/css/chunk-vendors.cebe6ab9.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/simulation-platform/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/simulation-platform/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ccd60b18e3b6d5738184bd0b20bafecc",
+    "url": "/simulation-platform/img/bottom_logo.ccd60b18.png"
+  },
+  {
+    "revision": "0e651ff6c9928dad413931ce4ed2057b",
+    "url": "/simulation-platform/img/login_bg.0e651ff6.jpg"
+  },
+  {
+    "revision": "c6fc28c94a950afd7302122eaa91dfcb",
+    "url": "/simulation-platform/img/logo1.c6fc28c9.png"
+  },
+  {
+    "revision": "40bc8856659194825aaa8f8aaf9b805f",
+    "url": "/simulation-platform/img/logo2.40bc8856.png"
+  },
+  {
+    "revision": "54875e89cb358bb873fd7e18260b172a",
+    "url": "/simulation-platform/img/wxgzh.54875e89.jpg"
+  },
+  {
+    "revision": "1c526ce7ed8cac2fed1a7c4c266f0d6f",
+    "url": "/simulation-platform/index.html"
+  },
+  {
+    "revision": "a8dc8e4df81d44e19667",
+    "url": "/simulation-platform/js/app.7046ba23.js"
+  },
+  {
+    "revision": "46b7c3fa2cb38303beb9",
+    "url": "/simulation-platform/js/chunk-57912280.2c1d2fdb.js"
+  },
+  {
+    "revision": "3198f9f80fd0e564c050",
+    "url": "/simulation-platform/js/chunk-vendors.d62e560a.js"
+  },
+  {
+    "revision": "cb132482ad62e48ff35dd7246a42b5fa",
+    "url": "/simulation-platform/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simulation-platform/robots.txt"
+  }
+]);
